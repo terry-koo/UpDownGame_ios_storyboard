8f13236fc9fdeac5482510342197d721d0305cfd
terry-koo <xpflxhfl94@naver.com>
2022-02-13 17:52:19 +0900

    Auto Layout

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider){
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton){
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ Sender:UIButton){
+        print("touchUpResetButton")
+    }
+    
+    
 }
 
